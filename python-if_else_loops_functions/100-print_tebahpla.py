0948ef8f819a2b54c5b43ebb7ffbd2b1046dd971
Gigi-Corlay <11489@holbertonstudents.com>
2026-01-16 10:06:05 +0000

    Print alphabet in reverse, alternating lowercase and uppercase

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 for i in range(26):
-    if i % 2 == 0:
-        lowercase = chr(122 - i)
-        print(lowercase, end='')
-    else:
-        uppercase = chr(90 - (i // 2) *  2 - 1)
-        print(uppercase, end='')
+    print("{:c}".format(122 - i if i % 2 == 0 else 90 - (i // 2) * 2 - 1), end='')
