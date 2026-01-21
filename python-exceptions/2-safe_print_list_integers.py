4173d745a23d8dbf7c7cd0793c5ce70db4d56765
Gigi-Corlay <11489@holbertonstudents.com>
2026-01-21 14:40:30 +0100

    Fix safe_print_list_integers: handle IndexError and skip non-integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,13 +2,12 @@
 def safe_print_list_integers(my_list=[], x=0):
     count_element = 0
     i = 0
+
     while i < x:
         try:
             print("{:d}".format(my_list[i]), end="")
             count_element += 1
-        except IndexError:
-            break
-        except (TypeError, ValueError):
+        except (ValueError, TypeError):
             pass
         i += 1
 
