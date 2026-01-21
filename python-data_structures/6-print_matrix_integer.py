88fc126aa68432a1565237c5b009ca16d22ae803
GigiCorlay <11489@holbertonstudents.com>
2026-01-21 07:42:43 +0100

    Enhance print_matrix_integer to handle empty matrix
    Added a check for an empty matrix and adjusted print logic.

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" ")
+    if matrix == [[]]:
         print()
+        return
+    for i in matrix:
+        for j in range(len(i)):
+            if j != len(i) - 1:
+                print("{}".format(i[j]), end=" ")
+            else:
+                print("{}".format(i[j]))
