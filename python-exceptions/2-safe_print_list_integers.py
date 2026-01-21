eb4782f40dbb652f35002a8ec82ae65c04aa8999
Gigi-Corlay <11489@holbertonstudents.com>
2026-01-21 14:30:05 +0100

    Implement safe_print_list_integers: prints only integers using try/except

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count_element = 0
+    i = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count_element += 1
+        except IndexError:
+            break
+        except (TypeError, ValueError):
+            pass
+        i += 1
+
+    print()
+    return count_element
