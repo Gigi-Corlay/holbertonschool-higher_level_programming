a73b237cc084acbfab0c293c6a73225d5797018d
Gigi-Corlay <11489@holbertonstudents.com>
2026-01-22 21:55:22 +0100

    Add matrix_divided function with input validation

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,34 @@
+def matrix_divided(matrix, div):
+    # Validate matrix
+    if (
+        not isinstance(matrix, list)
+        or not matrix
+        or not all(isinstance(row, list) for row in matrix)
+        or not all(
+            isinstance(n, (int, float))
+            for row in matrix
+            for n in row
+        )
+    ):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
+
+    # Validate row sizes
+    if len({len(row) for row in matrix}) != 1:
+        raise TypeError(
+            "Each row of the matrix must have the same size"
+        )
+
+    # Validate divisor
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    # Divide and round
+    return [
+        [round(n / div, 2) for n in row]
+        for row in matrix
+    ]
