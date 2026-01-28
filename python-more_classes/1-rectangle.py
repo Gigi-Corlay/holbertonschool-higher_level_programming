cacf8b63dee964a431f229a1590fc296d77c5218
Gigi-Corlay <11489@holbertonstudents.com>
2026-01-28 11:12:23 +0100

    Add Rectangle class with private width and height, proper getters and setters

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle class with private width and height attributes,
+and provides property getters and setters to access and modify them safely.
+"""
+
+
+class Rectangle:
+    """
+    This class defines a rectangle by its width and height.
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__height = value
