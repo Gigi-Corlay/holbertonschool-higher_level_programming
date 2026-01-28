2045a28490d22bff5480335bda15fb034591c8d6
Gigi-Corlay <11489@holbertonstudents.com>
2026-01-28 09:58:19 +0100

    Create empty Rectangle class with documentation

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""
+This module defines an empty Rectangle class.
+"""
+
+
 class Rectangle:
+    """
+    This class represents a rectangle.
+
+    It is currently empty and serves as a template for future
+    methods and attributes related to a rectangle.
+    """
     pass
