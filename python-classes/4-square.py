68e44aa37d6149bcf6d65f1d8b73de2448a6ca97
Gigi-Corlay <11489@holbertonstudents.com>
2026-01-28 09:38:17 +0100

    Add getter and setter for size attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class with a private size attribute
+and provides methods to access and modify it safely.
+"""
+
+
 class Square:
-    """Defines a square with a private size attribute"""
+    """
+    This class defines a square by its size.
+    """
 
     def __init__(self, size=0):
         self.size = size 
