fd554d23e6f1f39a104d69ecbf263b44bf79b647
Gigi-Corlay <11489@holbertonstudents.com>
2026-02-03 10:50:36 +0100

    Implement Rectangle class with private attributes, validation, area, and custom __str__

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module defining BaseGeometry and Rectangle classes
+"""
+
+
+class BaseGeometry:
+    """
+    Base class for geometry objects.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class with width and height
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
