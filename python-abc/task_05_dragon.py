e8c4e1538e932264701063652445e10401c9524d
Gigi-Corlay <11489@holbertonstudents.com>
2026-02-04 10:42:23 +0100

    Create Dragon class with SwimMixin and FlyMixin

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+"""
+Defines mixins and a Dragon class.
+"""
+
+
+class SwimMixin:
+    """
+    Mixin class adding swimming ability.
+    """
+
+    def swim(self):
+        print("The creature swims!")
+
+
+class FlyMixin:
+    """
+    Mixin class adding flying ability.
+    """
+    def fly(self):
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    """
+    Dragon class that can swim, fly, and roar.
+    """
+    def roar(self):
+        print("The dragon roars!")
+
+
+if __name__ == "__main__":
+    draco = Dragon()
+    draco.swim()
+    draco.fly() 
+    draco.roar()
