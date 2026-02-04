a9f46c9b17fe3bdc980e71916a859e38f8259c53
Gigi-Corlay <11489@holbertonstudents.com>
2026-02-04 10:51:14 +0100

    Create CountedIterator class with item counting

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""Defines a CountedIterator class."""
+"""
+Defines a CountedIterator class.
+"""
+
 
 class CountedIterator:
-    """Iterator that counts the number of items iterated."""
+    """
+    Iterator that counts the number of items iterated.
+    """
 
     def __init__(self, some_iterable):
-        """Initialize the iterator and counter."""
         self.iterator = iter(some_iterable)
         self.count = 0
 
-    def get_count(self):
-        """Return the number of items iterated so far."""
-        return self.count
-
     def __iter__(self):
-        """Return the iterator itself."""
         return self
 
     def __next__(self):
-        """Return the next item and increment the counter."""
+        item = next(self.iterator)
         self.count += 1
-        return next(self.iterator)
+        return item
+
+    def get_count(self):
+        return self.count
