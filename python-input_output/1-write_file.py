c13904793f4b7139bac4a93434efc41e4b2123d1
Gigi-Corlay <11489@holbertonstudents.com>
2026-02-09 13:47:53 +0100

    Add write_file function with UTF-8 support and character count return

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module that defines a function to write a string to a UTF-8 text file
+and return the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Write a string to a UTF-8 text file and return the number of characters written.
+
+    Args:
+        filename (str): Name of the file to write to.
+        text (str): The text to write into the file.
+
+    Returns:
+        int: Number of characters written to the file.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        nb_char = f.write(text)
+    return nb_char
