c0436330bea314ac1dc37041128b81dfcfb30410
Gigi-Corlay <11489@holbertonstudents.com>
2026-02-10 10:42:23 +0100

    Implement pascal_triangle function with comments

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Function that returns Pascal's triangle of size n
+"""
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing
+    the Pascal's triangle of n.
+
+    Args:
+        n (int): number of rows of the triangle
+
+    Returns:
+        list of lists of integers
+    """
+
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        if i >= 2:
+            prev_row = triangle[i - 1]
+
+            for j in range(1, i):
+                row[j] = prev_row[j - 1] + prev_row[j]
+
+        triangle.append(row)
+
+    return triangle
