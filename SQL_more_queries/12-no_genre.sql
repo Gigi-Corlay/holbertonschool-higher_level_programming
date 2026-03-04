15eee603facf1bff5d5146c0f45d3dd9c61d048e
Gigi-Corlay <11489@holbertonstudents.com>
2026-03-04 18:21:13 +0100

    We only want shows that have no genre

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,11 @@
+-- List all TV shows including those without a genre
+-- Select the requested columns: tv_shows.title and tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+-- Start from the tv_shows table
+FROM tv_shows
+-- Use LEFT JOIN to include shows that may not have a genre
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+-- Filter only shows without a genre
+WHERE tv_show_genres.genre_id IS NULL
+-- Sort by ascending tv_shows.title and tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
