7795cf90824797acf47eb0a34db833ef81a282e7
Gigi-Corlay <11489@holbertonstudents.com>
2026-03-04 09:57:27 +0100

    SQL_introduction: select score and name from second_table where name is not NULL or empty ordered by descending score

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,4 @@
+-- Selects score and name from second_table where name is not NULL or empty, ordered by descending score
 SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL AND name != ''
