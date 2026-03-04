cee0ece0afbb9dcf8afd504c3bc48e7cf4b0f57f
Gigi-Corlay <11489@holbertonstudents.com>
2026-03-04 09:55:40 +0100

    SQL_introduction: select score and count of rows from second_table grouped by score ordered by number descending

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,4 @@
+-- Selects score and the number of occurrences from second_table grouped by score and ordered by number descending
 SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
