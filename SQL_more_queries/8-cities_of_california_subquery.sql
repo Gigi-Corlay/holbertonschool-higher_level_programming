afec354f8c845dac9b29e8e8b5d1cb73a9e95ce9
Gigi-Corlay <11489@holbertonstudents.com>
2026-03-04 17:14:02 +0100

    Lists all the cities of California (id and name only)

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
--- lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT *
+-- Lists all the cities of California (id and name only)
+SELECT id, name
 FROM cities
 WHERE state_id = (
     SELECT id
