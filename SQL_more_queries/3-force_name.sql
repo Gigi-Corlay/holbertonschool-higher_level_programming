4449d1f2de5a6a7aca89dc30e9e89ac6447d8191
Gigi-Corlay <11489@holbertonstudents.com>
2026-03-04 12:21:03 +0100

    SQL_more_queries: create table force_name with id and non-null name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- Creates table force_name with id as primary key and name that cannot be null
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT PRIMARY KEY,
+    id INT,
     name VARCHAR(256) NOT NULL
 );
