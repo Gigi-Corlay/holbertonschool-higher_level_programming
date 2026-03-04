7de26b7f2466c32b27da2ee85229b05eb78aee14
Gigi-Corlay <11489@holbertonstudents.com>
2026-03-04 10:33:01 +0100

    SQL_more_queries: create user_0d_1, grant all privileges, and display the privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
+-- Creates user_0d_1 if it does not exist, grants all privileges, and displays the user's privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
