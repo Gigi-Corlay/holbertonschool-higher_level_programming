b31d963222ee2b4e49abe047f4ee2b6cf458332d
Gigi-Corlay <11489@holbertonstudents.com>
2026-03-04 17:47:18 +0100

    List all TV shows that have at least one genre linked -- Select the requested columns: tv_shows.ti

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,10 @@
+-- List all TV shows that have at least one genre linked
+-- Select the requested columns: tv_shows.title and tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+-- Start from the tv_shows table
+FROM tv_shows
+-- Link each tv_show to its genres via tv_show_genres
+INNER JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.tv_show_id
+-- Sort by ascending tv_shows.title and tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
