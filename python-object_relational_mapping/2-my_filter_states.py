35cfbbb6edd38c0e9a79e9513a18570ea1899a46
Gigi-Corlay <11489@holbertonstudents.com>
2026-03-05 21:50:56 +0000

    Displays the first state in the states table where name matches the argument

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -6,32 +6,37 @@ Displays the first state in the states table where name matches the argument
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
-    # Get arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_name = sys.argv[4]
+    state = sys.argv[4]
 
-    # Connect to MySQL
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
         passwd=password,
-        db=database
+        db=database,
+        charset="utf8"
     )
 
     cursor = db.cursor()
 
-    # Use parameterized query and fetch only one row
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC LIMIT 1"
-    cursor.execute(query, (state_name,))
+    query = (
+        "SELECT states.id, states.name "
+        "FROM states "
+        "WHERE name = BINARY '{}' "
+        "ORDER BY states.id ASC"
+    ).format(state)
 
-    row = cursor.fetchone()  # Fetch only the first matching row
-    if row:
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
         print(row)
 
-    # Close cursor and connection
     cursor.close()
     db.close()
