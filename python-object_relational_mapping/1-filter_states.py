2d482a9f6384477bc2aa707d2603afb4b63da083
Gigi-Corlay <11489@holbertonstudents.com>
2026-03-05 21:25:37 +0100

    Lists all states with a name starting with N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,10 +7,12 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    # Récupère les arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
+    # Connexion à MySQL
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -20,12 +22,17 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    cursor.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
-    )
 
+    # Exécute la requête
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+
+    # Récupère tous les résultats dans 'rows'
+    rows = cursor.fetchall()
+
+    # Affiche chaque tuple (id, name)
     for row in rows:
         print(row)
 
+    # Ferme le curseur et la connexion
     cursor.close()
     db.close()
