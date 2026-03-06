56c80a0f14a15cb84d7029569871ea94d36d4723
Gigi-Corlay <11489@holbertonstudents.com>
2026-03-06 14:10:57 +0000

    Add SQL files and update 4-cities_by_state script

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,40 +1,29 @@
 #!/usr/bin/python3
 """
-Lists all cities from the database hbtn_0e_4_usa
-with their corresponding state name.
+Lists all cities from a MySQL database with their state name.
 """
 
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./4-cities_by_state.py <user> <password> <database>")
-        sys.exit(1)
 
-    user = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Connect to MySQL
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=user,
-        passwd=password,
-        db=database
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
     cursor = db.cursor()
 
-    # Execute one query with JOIN to get cities and their states
-    query = """
-    SELECT cities.id, cities.name, states.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    ORDER BY cities.id ASC;
-    """
-    cursor.execute(query)
+    cursor.execute("""
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC
+    """)
 
     rows = cursor.fetchall()
 
@@ -43,4 +32,3 @@ if __name__ == "__main__":
 
     cursor.close()
     db.close()
-  
