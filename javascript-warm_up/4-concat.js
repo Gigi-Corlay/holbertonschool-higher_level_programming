79ef9f2cd4ffe5f56c1e985b1f906fb73c6d09bf
Gigi-Corlay <11489@holbertonstudents.com>
2026-03-20 14:39:57 +0000

    Create a sentence

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,10 +1,3 @@
 #!/usr/bin/node
 
-// Get the first argument passed to the script
-const arg1 = process.argv[2];
-
-// Get the second argument
-const arg2 = process.argv[3];
-
-// Print the arguments in the format "<arg1> is <arg2>"
-console.log(`${arg1} is ${arg2}`);
+console.log(process.argv[2] + ' is ' + process.argv[3]);
