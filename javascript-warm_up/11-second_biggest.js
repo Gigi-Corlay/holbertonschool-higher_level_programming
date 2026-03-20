defdc0da6e565ca6ebce0e9ebe2bc23a3a8ceec4
Gigi-Corlay <11489@holbertonstudents.com>
2026-03-20 17:27:33 +0000

    Correction : 11. Second biggest!

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,25 +1,11 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2).map(Number);
-
-if (args.length < 2) {
-  console.log(0);
+if (process.argv.length <= 3) {
+    console.log(0);
 } else {
-  let max = args[0];
-  let second = args[0];
+    const args = process.argv.slice(2).map(Number);
 
-  for (let i = 1; i < args.length; i++) {
-    if (args[i] > max) {
-      second = max;
-      max = args[i];
-    } else if (args[i] > second && args[i] < max) {
-      second = args[i];
-    }
-  }
+    args.sort((a, b) => b - a);
 
-  if (second === max) {
-    console.log(max);
-  } else {
-    console.log(second);
-  }
+    console.log(args[1]);
 }
