9c3c49964924d0fa48eaefd0c938727cdfccfdb8
Gigi-Corlay <11489@holbertonstudents.com>
2026-03-20 14:53:29 +0000

    5. An Integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2]);
 
-if (Number.isInteger(num)) {
-  console.log('My number: ' + num);
-} else {
-  console.log('Not a number');
-}
+const num = parseInt(process.argv[2], 10);
+
+console.log(!isNaN(num) ? `My number: ${num}` : 'Not a number');
