3ef3d357d3cc9187a503ae818be0a853422e08f3
Gigi-Corlay <11489@holbertonstudents.com>
2026-03-20 17:16:11 +0000

    Correction task10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-function factorial(n) {
-  if (isNaN(n) || n <= 0) {
+function factorial (n) {
+  if (isNaN(n) || n === 0 || n === 1) {
     return 1;
   }
   return n * factorial(n - 1);
 }
 
-const n = parseInt(process.argv[2]);
-console.log(factorial(n));
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
