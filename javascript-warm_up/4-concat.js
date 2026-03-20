6cff9f03d9d95be3615cf3ad8e03e7d2283496fe
Gigi-Corlay <11489@holbertonstudents.com>
2026-03-20 14:37:03 +0000

    Correction :Create a sentence

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 
+// Get the first argument passed to the script
 const arg1 = process.argv[2];
+
+// Get the second argument
 const arg2 = process.argv[3];
 
+// Print the arguments in the format "<arg1> is <arg2>"
 console.log(`${arg1} is ${arg2}`);
