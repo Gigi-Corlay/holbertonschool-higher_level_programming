647ed838fa9c39191aeb64331ff754eb71958a6a
Gigi-Corlay <11489@holbertonstudents.com>
2026-03-24 18:14:12 +0100

    Task 02: Add dynamic items page using Jinja loops and conditionals

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import json
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/items')
+def items():
+    with open('items.json') as f:
+        data = json.load(f)
+        items_list = data["items"]
+
+    return render_template('items.html', items=items_list)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
